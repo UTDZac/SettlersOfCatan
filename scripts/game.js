ce6383aaf25ba7c04750f847c767bbbbc68b8a24
Zac Elsik <shadowgripper@gmail.com>
2013-04-28 16:37:37 -0500

    Data section added to game javascript.

---
+++ scripts/game.js
@@ -0,0 +1,51 @@
+// Resource Card Enum
+// 0 - Wood
+// 1 - Brick
+// 2 - Sheep
+// 3 - Wheat
+// 4 - Ore
+
+// Development Card Enum
+// 0 - Knight
+// 1 - Victory Point
+// 2 - Road Building
+// 3 - Year of Plenty
+// 4 - Monopoly
+
+var catan = {
+data: {
+	p1: {
+		pieces: {
+			settlements: 5,
+			cities: 4,
+			roads: 15
+		},
+		resources: [],
+		knights: 0,
+		dev: [],
+		points: 0
+	},
+	p2: {
+		pieces: {
+			settlements: 5,
+			cities: 4,
+			roads: 15
+		},
+		resources: [],
+		knights: 0,
+		dev: [],
+		points: 0
+	},
+	board: {
+		tiles: {},
+		intersections: {},
+		ports: {},
+		roads: {}
+	},
+	bank: {
+		dev: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+			1, 1, 1, 1, 1,
+			2, 2, 3, 3, 4, 4]
+	}
+}
+}
